41b9103fbadd6a3689cf11da6b76ed057db62dda
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2025-05-16 21:25:37 +0300

    Demo custom authorizer - check YAML

--- iam-role-policy-attachments.tf
+++ iam-role-policy-attachments.tf
@@ -4,6 +4,7 @@
   This policy provides the necessary permissions for the Lambda function to execute.
   - role: The name of the IAM role to attach the policy to.
   - policy_arn: The ARN of the IAM policy to attach to the role.
+  
 */
 resource "aws_iam_role_policy_attachment" "service_api_check_handler_role_attachment" {
   role       = aws_iam_role.service_api_check_handler_role.name
